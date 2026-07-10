407744cb975ce874364d0ac6abf583605e5f00e7
Siraj alkhadwri <seragalkhaduri@gmail.com>
2026-07-10 18:13:22 +0200

    feat: Add pre-seeded SQLite database and default connection fallback

--- uimp-core/config/database.php
+++ uimp-core/config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION') === 'pgsql' ? 'mysql' : env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION') === 'pgsql' ? 'sqlite' : env('DB_CONNECTION', 'sqlite'),
 
     /*
     |--------------------------------------------------------------------------
@@ -34,7 +34,7 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => env('DB_DATABASE', base_path('../database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
             'busy_timeout' => null,
